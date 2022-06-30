36a431a07693165225ff7a0aff0c3852272f0a2f
indev <bdmitry450@gmail.com>
2022-06-30 10:18:48 +0300

    Fix block collision

--- PhysicalEntity.cpp
+++ PhysicalEntity.cpp
@@ -58,6 +58,7 @@ namespace Game {
                         }
 
                         speed.x = 0;
+                        return;
                     } else {
                         if (speed.y > 0) {
                             hitbox.y = y*TILE_HEIGHT - hitbox.h;
@@ -68,6 +69,7 @@ namespace Game {
                         }
 
                         speed.y = 0;
+                        return;
                     }
                 }
             }
